1d4ffa921bda135742ad98fc3d3b82bc47646d37
JC <jc@jcmbp.local>
2016-01-04 16:16:44 -0800

    refactor: move yelp code from routes.js to yelp.js

--- server/routes.js
+++ server/routes.js
@@ -1,29 +1,6 @@
-var yelp = require('./yelp');
-var Firebase = require("firebase");
-var restaurantsRef = new Firebase("https://vivid-inferno-1656.firebaseio.com/");
-var hash = require('string-hash');
-
-var formatData = function(data) {
-  var formattedData = {};
-  data.businesses.forEach(function(restaurant) {
-    // organize restaurants by a key, which is a hash of the name. This will make it easier to look up a particular
-    // restaurant later when we want to update its score.
-    var hashedName = hash(restaurant.name);
-    formattedData[hashedName] = restaurant;
-    formattedData[hashedName]['eloRating'] = 1400;
-  });
-  return formattedData;
-};
+var yelpSearch = require('./yelp');
 
 module.exports = function (app, express) {
-  app.post('/test', function(req, res) {
-    yelp(req.body.location).then(function(data) {
-      data = formatData(data);
-      restaurantsRef.set(data)
-      res.json(data);
-    }).catch(function(err) {
-      console.log(err);
-    });
-  });
+  app.post('/test', yelpSearch);
 };
 
--- server/yelp.js
+++ server/yelp.js
@@ -1,10 +1,30 @@
 var Yelp = require('yelp');
-var yelpKeys = require('./config')
+var yelp_api_keys = require('./config')
+var yelp = new Yelp(yelp_api_keys);
+var Firebase = require("firebase");
+var restaurantsRef = new Firebase("https://vivid-inferno-1656.firebaseio.com/");
+var hash = require('string-hash');
 
-var yelp = new Yelp(yelpKeys);
+var formatData = function(data) {
+  var formattedData = {};
+  data.businesses.forEach(function(restaurant) {
+    // organize restaurants by a key, which is a hash of the name. This will make it easier to look up a particular
+    // restaurant later when we want to update its score.
+    var hashedName = hash(restaurant.name);
+    formattedData[hashedName] = restaurant;
+    formattedData[hashedName]['eloRating'] = 1400;
+  });
+  return formattedData;
+};
 
-var yelpSearch = function(location) {
-  return yelp.search({ term: 'food', location: location });
+var yelpSearch = function(req, res) {
+  yelp.search({ term: 'food', location: req.body.location}).then(function(data) {
+    data = formatData(data);
+    restaurantsRef.set(data)
+    res.json(data);
+  }).catch(function(err) {
+    console.log(err);
+  });
 };
 
 module.exports = yelpSearch;
